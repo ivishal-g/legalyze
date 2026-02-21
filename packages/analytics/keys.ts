c38bb6d28aa5e32ec11e35910d6c74b0368259eb
ivishal-g <gaikwad8vishal@gmail.com>
2026-02-21 13:31:06 +0530

    feat

--- packages/analytics/keys.ts
+++ packages/analytics/keys.ts
@@ -4,9 +4,9 @@ import { z } from "zod";
 export const keys = () =>
   createEnv({
     client: {
-      NEXT_PUBLIC_POSTHOG_KEY: z.string().startsWith("phc_"),
-      NEXT_PUBLIC_POSTHOG_HOST: z.url(),
-      NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().startsWith("G-").optional(),
+      NEXT_PUBLIC_POSTHOG_KEY: z.string().optional(),
+      NEXT_PUBLIC_POSTHOG_HOST: z.string().optional(),
+      NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().optional(),
     },
     runtimeEnv: {
       NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
